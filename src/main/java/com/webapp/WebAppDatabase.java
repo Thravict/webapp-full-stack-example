de958c1f01dc716c832314800d08bbac29258c6a
jakob.friedrich <jakob.friedrich@cgm.com>
2022-12-09 14:35:24 +0100

    Change Class names for conventions, add thymeleaf dependency, add welcome html file,

--- src/main/java/com/webapp/WebAppDatabase.java
+++ src/main/java/com/webapp/WebAppDatabase.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class webAppDatabase {
+public class WebAppDatabase {
 
     public static void main(String[] args) {
-        SpringApplication.run(webAppDatabase.class, args);
+        SpringApplication.run(WebAppDatabase.class, args);
     }
 
 }
