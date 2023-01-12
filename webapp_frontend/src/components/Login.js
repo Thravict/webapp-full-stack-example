580149353878e1787f0d3e87ac0a9e8979e0a990
melanie.gille <Melanie.Gille@cgm.com>
2023-01-12 08:29:15 +0100

    Fonts edited

--- webapp_frontend/src/components/Login.js
+++ webapp_frontend/src/components/Login.js
@@ -6,20 +6,25 @@ function Login() {
 	return(
 	<html>
         <body>
+          <h1 className="Login-title">Welcome to the Game Pass Portal</h1>
           <div className="Login-container">
+            
             <div className="Login-head">
-              <h1>Login</h1>
+              <h1>Please enter your credentials</h1>
             </div>
             <form action="#">
-            <div className="username">
-            <label>Email Address</label>
-            <input type="text"></input>
+            <div className="email">
+            <label for="Email Address" className="email-label">Email Address</label>
+            <input className="email-field" type="email" autocomplete="on"></input>
             </div>
             <div className="password">
-
+            <label for="Password" className="password-label">Password</label>
+            <input className="password-field" type="password"></input>
             </div>
             <div className="Submit-btn">
-              <input type="submit" value="Submit"></input>
+              <button className="Submit-btn">
+                <span>Login</span>
+              </button>
             </div>
             </form>
           </div>
