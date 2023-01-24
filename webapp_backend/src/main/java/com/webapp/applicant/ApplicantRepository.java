c2ec139084db74826f73a117a6f3756affdea8e6
jakob.friedrich <jakob.friedrich@cgm.com>
2023-01-24 07:41:12 +0100

    Add post for Boolean if email matches or not

--- webapp_backend/src/main/java/com/webapp/applicant/ApplicantRepository.java
+++ webapp_backend/src/main/java/com/webapp/applicant/ApplicantRepository.java
@@ -1,9 +1,10 @@
 package com.webapp.applicant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
 
-
+    Optional<Applicant> findApplicantByEmail(String applicantEmail);
 
 }
