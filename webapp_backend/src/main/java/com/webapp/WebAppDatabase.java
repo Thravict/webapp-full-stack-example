93ce98084873a45d3be1b709241d87547aae1a24
jakob.friedrich <jakob.friedrich@cgm.com>
2023-01-27 12:52:26 +0100

    automatically add ids to new applicants

--- webapp_backend/src/main/java/com/webapp/WebAppDatabase.java
+++ webapp_backend/src/main/java/com/webapp/WebAppDatabase.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebAppDatabase {
+public class  WebAppDatabase {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAppDatabase.class, args);
