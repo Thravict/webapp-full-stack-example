eaca9e6a972f9bad3b2d101096658d4f5f205397
melanie.gille <Melanie.Gille@cgm.com>
2023-02-09 23:35:29 +0100

    Footer created

---
+++ webapp_frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+
+    return (
+        <h2 
+        style={{
+            Color:"white",
+            backgroundColor:"black",
+        }}>
+        © 2023 Jameto
+        </h2>
+    )
+}
+
+export default Footer;
