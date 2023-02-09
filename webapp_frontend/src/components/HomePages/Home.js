eb3c3deea04a24c4f1175b9552fdb132e4684aa5
melanie.gille <Melanie.Gille@cgm.com>
2023-02-09 22:58:59 +0100

    new file structure

---
+++ webapp_frontend/src/components/HomePages/Home.js
@@ -0,0 +1,19 @@
+import "./Home.css"
+//Logo Banner
+import Logo from "../Logo";
+import Navbar from "./Navbar";
+
+function Home() {
+
+    return (
+        <body>
+        <Logo></Logo>
+        <Navbar></Navbar>
+        <div>
+            Test
+        </div>
+        </body>
+    )
+}
+
+export default Home;
---
+++ webapp_frontend/src/components/HomePages/Navbar.js
@@ -0,0 +1,47 @@
+//Routing
+import {Link} from "react-router-dom";
+//CSS
+import "./Navbar.css"
+//Icons
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHome, faCircleUser, faGear, faBars, faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+
+    return (
+        <ul className="navbar">
+            <li className="navbar-list-home">
+            <Link to="/home">
+            <FontAwesomeIcon icon={faHome} />&nbsp;
+            Home
+            </Link>
+            </li>
+            <li className="navbar-list-profile">
+            <Link to="/profile">
+            <FontAwesomeIcon icon={faCircleUser} />&nbsp;
+            Profile
+            </Link>
+            </li>
+            <li className="navbar-list-dashboard">
+            <Link to="/dashboard">
+            <FontAwesomeIcon icon={faBars} />&nbsp;
+            Dashboard
+            </Link>
+            </li>
+            <li className="navbar-list-settings">
+            <Link to="/settings">
+            <FontAwesomeIcon icon={faGear} />&nbsp;
+            Settings
+            </Link>
+            </li>
+            <li className="navbar-list-logout">
+            <Link to="/login">
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />&nbsp;
+            Logout
+            </Link>
+            </li>
+        </ul>
+    )
+}
+
+export default Navbar;
