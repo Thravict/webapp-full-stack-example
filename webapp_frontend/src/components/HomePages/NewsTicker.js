c82beb9e560f2aa2b479aad252916eb7b968ea6f
melanie.gille <Melanie.Gille@cgm.com>
2023-02-20 13:15:55 +0100

    NewsTicker added

---
+++ webapp_frontend/src/components/HomePages/NewsTicker.js
@@ -0,0 +1,20 @@
+import "./NewsTicker.css"
+
+function Settings() {
+
+    return (
+        <body className="NewsTicker-body">
+        <div className="news-container">
+            <div className="title">
+                Breaking News
+            </div>
+            <div className="text-animation">
+            <p> Apple TV releases trailer to 'Tetris' streaming March 31. </p>
+            <p>Hogwarts Legacy has no plans for DLC - Big Loss? </p>
+            </div>
+        </div>
+        </body>
+    )
+}
+
+export default Settings;
